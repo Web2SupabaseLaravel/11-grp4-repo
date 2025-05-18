401d6dd32a432aeec6ece3059aa869548353a292
Ruaa_Saleh <s12220826@stu.najah.edu>
2025-05-19 00:10:14 +0300

    Update Users.php

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+
+    protected $table = 'users';
+    protected $primarykey='user_id';
+    public $timestamps = true;
+    public $incrementing = false;          
+    protected $keyType = 'string';
+
+
+    protected $fillable = [
+        'email', 'password',
+        'roletype','restaurant_id','
+        social_login', 'is_active',
+        'created_at', 'updated_at'
+    ];
+
+}
